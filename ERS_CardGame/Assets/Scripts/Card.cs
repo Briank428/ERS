11d43c88cbfc8369264bf35b430453554fdbe406
Briank428 <44040150+Briank428@users.noreply.github.com>
2019-05-30 21:10:09 -0400

    ;amsc

--- ERS_CardGame/Assets/Scripts/Card.cs
+++ ERS_CardGame/Assets/Scripts/Card.cs
@@ -1,7 +1,12 @@
-﻿public class Card
+﻿using System.Collections;
+using UnityEngine;
+public class Card : MonoBehaviour
 {
     public int value;
     public string suit;
+    public Transform pilePosition, handPosition, currentPos;
+
+    private float speed;
 
     public static string[] suits =
     {
@@ -11,14 +16,27 @@
         "DIAMONDS"
     };
     
-    public Card(int v, string s)
+    public void CardInit(int v, string s)
     {
         value = v;
         suit = s;
+        currentPos = pilePosition;
     }
     public bool IsFaceCard()
     {
         if (value > 10) return true;
         return false;
     }
+    public void SetPlayerPos(Transform t)
+    {
+        handPosition = t;
+    }
+    public void SetPosition(Transform t)
+    {
+        currentPos = t;
+        Transform oldPos = pilePosition;
+        if (currentPos == pilePosition) oldPos = handPosition;
+        Move()
+    }
+
 }
