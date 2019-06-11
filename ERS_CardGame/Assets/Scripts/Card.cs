fa321965f7c97f93f387f5cddeadc851d0d57f9d
Briank428 <44040150+Briank428@users.noreply.github.com>
2019-06-11 11:07:27 -0400

    dd

--- ERS_CardGame/Assets/Scripts/Card.cs
+++ ERS_CardGame/Assets/Scripts/Card.cs
@@ -6,22 +6,25 @@ public class Card : MonoBehaviour
     public string suit;
     public Transform handPosition, currentPos, oldPos;
     public static Transform pilePosition;
-    public static Sprite back;
+    [SerializeField]
+    public Sprite back, front;
     private float speed;
     private Rigidbody2D rb;
+    private SpriteRenderer currentSprite;
 
-    public static string[] suits =
-    {
-        "CLUBS",
-        "HEARTS",
-        "SPADES",
-        "DIAMONDS"
-    };
-    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        front = GetComponent<SpriteRenderer>().sprite;  
+        back = GameManager.back;
         currentPos = null;
+        currentSprite = GetComponent<SpriteRenderer>();
+        currentSprite.sprite = back;
+    }
+
+    public void Flip()
+    {
+        //flip 90, switch sprite, flip back
     }
     public bool IsFaceCard()
     {
